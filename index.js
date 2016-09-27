61e70c2d26a632ad9251348a0625ed62974f60ea
John Kennedy <johnwjken@gmail.com>
2016-09-27 14:58:44 +0100

    Refractor view pages to .hbs and refractored titles from donations.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const Hapi = require('hapi');
 var server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 4000 });
 
-server.register(require('inert'), err => {
+server.register([require('inert'), require('vision')], err => {
+
   if (err) {
     throw err;
   }
@@ -21,11 +22,12 @@ server.register(require('inert'), err => {
 
   server.route(require('./routes'));
 
-  server.start(err => {
+  server.start((err) => {
     if (err) {
       throw err;
     }
 
     console.log('Server listening at:', server.info.uri);
   });
+
 });
