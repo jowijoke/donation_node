b5823b2bce10c2cdc9fecf0e9fca3eb920d30f70
John Kennedy <johnwjken@gmail.com>
2016-09-28 10:54:27 +0100

    Created Accounts controller

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -3,7 +3,23 @@
 exports.home = {
 
   handler: (request, reply) => {
-    reply.view('main', {title: 'Welcome to Donations'});
+    reply.view('home', {title: 'Make a Donation'});
+  },
+
+};
+
+exports.report = {
+
+  handler: function (request, reply) {
+    reply.view('report', {title: 'Donations to Date',});
+  },
+
+};
+
+exports.donate = {
+
+  handler: function (request, reply) {
+    reply.redirect('/report');
   },
 
 };
