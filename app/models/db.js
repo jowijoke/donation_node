5536bd2d7de7c91f9720309371623d5b32d905db
John Kennedy <johnwjken@gmail.com>
2016-10-18 10:46:30 +0100

    Installed mongoose seeding and placeed a data.json file

--- app/models/db.js
+++ app/models/db.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 let dbURI =  'mongodb://localhost/donation';
+
 // 'mongodb://donationuser:donationuser@ds047940.mlab.com:47940/donation';
 
 if (process.env.NODE_ENV === 'production') {
@@ -14,6 +15,18 @@ mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', function () {
   console.log('Mongoose connected to ' + dbURI);
+  if (process.env.NODE_ENV != 'production') {
+    var seeder = require('mongoose-seeder');
+    const data = require('./data.json');
+    const Donation = require('./donation');
+    const User = require('./user');
+    seeder.seed(data, { dropDatabase: false, dropCollections: true }).then(dbData => {
+      console.log('preloading Test Data');
+      console.log(dbData);
+    }).catch(err => {
+      console.log(error);
+    });
+  }
 });
 
 mongoose.connection.on('error', function (err) {
