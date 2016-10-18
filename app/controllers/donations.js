95a2ce071d7399f8d63dbbdc4197c133b993e918
John Kennedy <johnwjken@gmail.com>
2016-10-18 11:01:06 +0100

    Create Candidate Reference in Donation Object

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -1,13 +1,20 @@
 'use strict';
 const Donation = require('../models/donation');
 const User = require('../models/user');
+const Candidate = require('../models/candidate');
 
 exports.home = {
 
   handler: (request, reply) => {
-    reply.view('home', { title: 'Make a Donation' });
+    Candidate.find({}).then(candidates => {
+      reply.view('home', {
+        title: 'Make a Donation',
+        candidates: candidates,
+      });
+    }).catch(err => {
+      reply.redirect('/');
+    });
   },
-
 };
 
 exports.report = {
@@ -32,13 +39,21 @@ exports.donate = {
     User.findOne({ email: userEmail }).then(user => {
       let data = request.payload;
       const donation = new Donation(data);
-      donation.donor = user._id;
-      return donation.save();
-    }).then(newDonation => {
-      reply.redirect('/report');
+      const rawCandidate = request.payload.candidate.split(',');
+      Candidate.findOne({
+        lastName: rawCandidate[0],
+        firstName: rawCandidate[1],
+      }).then(candidate => {
+        donation.donor = user._id;
+        donation.candidate = candidate._id;
+        donation.save().then(newDonation => {
+          reply.redirect('/report');
+        });
+      }).catch(err => {
+        reply.redirect('/');
+      });
     }).catch(err => {
       reply.redirect('/');
     });
   },
-
 };
