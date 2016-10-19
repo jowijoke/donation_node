41a25c9ea0abf65c5d5cd8e632613ec2f3869081
John Kennedy <johnwjken@gmail.com>
2016-10-19 11:02:21 +0100

    Place total in report, input amount & validate donate view

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -2,6 +2,7 @@
 const Donation = require('../models/donation');
 const User = require('../models/user');
 const Candidate = require('../models/candidate');
+const Joi = require('joi');
 
 exports.home = {
 
@@ -21,9 +22,14 @@ exports.report = {
 
   handler: function (request, reply) {
     Donation.find({}).populate('donor').populate('candidate').then(allDonations => {
+      let total = 0;
+      allDonations.forEach(donation => {
+        total += donation.amount;
+      });
       reply.view('report', {
         title: 'Donations to Date',
         donations: allDonations,
+        total:total,
       });
     }).catch(err => {
       reply.redirect('/');
@@ -34,26 +40,50 @@ exports.report = {
 
 exports.donate = {
 
-  handler: function (request, reply) {
-    var userEmail = request.auth.credentials.loggedInUser;
-    User.findOne({ email: userEmail }).then(user => {
-      let data = request.payload;
-      const donation = new Donation(data);
-      const rawCandidate = request.payload.candidate.split(',');
-      Candidate.findOne({
-        lastName: rawCandidate[0],
-        firstName: rawCandidate[1],
-      }).then(candidate => {
-        donation.donor = user._id;
-        donation.candidate = candidate._id;
-        donation.save().then(newDonation => {
-          reply.redirect('/report');
-        });
+  validate: {
+
+    payload: {
+      amount: Joi.number().required(),
+      method: Joi.string().required(),
+      candidate: Joi.string().required(),
+    },
+
+    options: {
+      abortEarly: false,
+    },
+
+    failAction: function (request, reply, source, error) {
+      Candidate.find({}).then(candidates => {
+        reply.view('home', {
+          title: 'Invalid Donation',
+          candidates: candidates,
+          errors: error.data.details,
+        }).code(400);
       }).catch(err => {
         reply.redirect('/');
       });
+    },
+  },
+
+  handler: function (request, reply) {
+    var userEmail = request.auth.credentials.loggedInUser;
+    let userId = null;
+    let donation = null;
+    User.findOne({ email: userEmail }).then(user => {
+      let data = request.payload;
+      userId = user._id;
+      donation = new Donation(data);
+      const rawCandidate = request.payload.candidate.split(',');
+      return Candidate.findOne({ lastName: rawCandidate[0], firstName: rawCandidate[1] });
+    }).then(candidate => {
+      donation.donor = userId;
+      donation.candidate = candidate._id;
+      return donation.save();
+    }).then(newDonation => {
+      reply.redirect('/report');
     }).catch(err => {
       reply.redirect('/');
     });
   },
+
 };
